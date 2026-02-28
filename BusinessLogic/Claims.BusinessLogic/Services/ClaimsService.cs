5fc9fef865c75921b26cb8710c7a8c17360f5546
Eugen Selbstreich <qduck@mail.ru>
2026-02-28 20:33:32 +0100

    add damage cost check

--- BusinessLogic/Claims.BusinessLogic/Services/ClaimsService.cs
+++ BusinessLogic/Claims.BusinessLogic/Services/ClaimsService.cs
@@ -5,33 +5,40 @@ namespace Claims.BusinessLogic.Services
 {
     public class ClaimsService : IClaimsService
     {
-        private readonly IClaimsRepository _repository;
+        private readonly IClaimsRepository _claimsRepository;
 
-        public ClaimsService(IClaimsRepository repository)
+        public ClaimsService(IClaimsRepository claimsRepository)
         {
-            _repository = repository;
+            _claimsRepository = claimsRepository;
         }
 
         public async Task<Result<IEnumerable<Claim>>> GetClaimsAsync()
         {
-            return await _repository.GetClaimsAsync();
+            return await _claimsRepository.GetClaimsAsync();
         }
 
         public async Task<Result<Claim>> GetClaimAsync(string id)
         {
-            return await _repository.GetClaimAsync(id);
+            return await _claimsRepository.GetClaimAsync(id);
         }
 
         public async Task<Result<Claim>> CreateClaimAsync(Claim claim)
         {
+            // TODO: Check for existing coverId?
+            const int maxDamageCost = 100000;
+            if (claim.DamageCost > maxDamageCost)
+            {
+                var error = new ArgumentException($"Damage cost must be less than {maxDamageCost}.");
+                return Result.FromException<Claim>(error);
+            }
             claim.Id = Guid.NewGuid().ToString();
-            await _repository.AddClaimAsync(claim);
-            return claim;
+            await _claimsRepository.AddClaimAsync(claim);
+            return Result.FromSuccess(claim);
         }
 
         public async Task<Result> DeleteClaimAsync(string id)
         {
-            return await _repository.DeleteClaimAsync(id);
+            return await _claimsRepository.DeleteClaimAsync(id);
         }
     }
 }
