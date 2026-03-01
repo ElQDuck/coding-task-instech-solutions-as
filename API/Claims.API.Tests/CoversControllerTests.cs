4f2029cd53c5e0aba5152dfb1f544dc147e78067
Eugen Selbstreich <qduck@mail.ru>
2026-03-01 20:32:46 +0100

    add api tests

--- API/Claims.API.Tests/CoversControllerTests.cs
+++ API/Claims.API.Tests/CoversControllerTests.cs
@@ -56,5 +56,74 @@ namespace Claims.Tests
             Assert.That(realValue!.Count, Is.EqualTo(0));
         }
 
+        [Test]
+        public async Task CoversController_GetCoverAsync_Success()
+        {
+            // Prepare
+            var cover = new Cover { Id = "1" };
+            var returnValue = Task.FromResult(Result.FromSuccess(cover));
+            _coversServiceMock.GetCoverAsync("1").Returns(returnValue);
+
+            // Act
+            var result = await _testee.GetCoverAsync("1");
+
+            // Assert
+            Assert.That(result.Result, Is.Not.Null);
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+            var okObjectResult = (OkObjectResult)result.Result!;
+            var realValue = (Cover)okObjectResult.Value!;
+            Assert.That(realValue.Id, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public async Task CoversController_CreateCoverAsync_Success()
+        {
+            // Prepare
+            var cover = new Cover { Id = "create-id" };
+            var returnValue = Task.FromResult(Result.FromSuccess(cover));
+            _coversServiceMock.CreateCoverAsync(cover).Returns(returnValue);
+
+            // Act
+            var result = await _testee.CreateCoverAsync(cover);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+            var okObjectResult = (OkObjectResult)result;
+            var realValue = (Cover)okObjectResult.Value!;
+            Assert.That(realValue.Id, Is.EqualTo("create-id"));
+        }
+
+        [Test]
+        public async Task CoversController_DeleteCoverAsync_Success()
+        {
+            // Prepare
+            _coversServiceMock.DeleteCoverAsync("del-1").Returns(Task.FromResult(Result.FromSuccess()));
+
+            // Act
+            var result = await _testee.DeleteCoverAsync("del-1");
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<NoContentResult>());
+        }
+
+        [Test]
+        public void CoversController_ComputePremium_Success()
+        {
+            // Prepare
+            var start = DateTime.UtcNow;
+            var end = start.AddDays(1);
+            _premiumComputeServiceMock.ComputePremium(start, end, CoverType.Tanker).Returns(12.34m);
+
+            // Act
+            var result = _testee.ComputePremiumAsync(start, end, CoverType.Tanker);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+            var ok = (OkObjectResult)result;
+            Assert.That(ok.Value, Is.EqualTo(12.34m));
+        }
+
     }
 }
