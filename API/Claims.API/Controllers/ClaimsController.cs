45568c412ce9f159ffa6985744fd7937cb6bc2b6
Eugen Selbstreich <qduck@mail.ru>
2026-03-01 15:35:52 +0100

    add comments

--- API/Claims.API/Controllers/ClaimsController.cs
+++ API/Claims.API/Controllers/ClaimsController.cs
@@ -2,21 +2,32 @@ using Claims.BusinessLogic.Entities;
 using Claims.BusinessLogic.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-// 
 namespace Claims.API.Controllers
 {
+    /// <summary>
+    /// The REST API controller for claims.
+    /// </summary>
     [ApiController]
     public class ClaimsController : ControllerBase
     {
         private readonly ILogger<ClaimsController> _logger;
         private readonly IClaimsService _claimsService;
 
+        /// <summary>
+        /// Initializes an instance of the <see cref="ClaimsController"/> class.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="claimsService"></param>
         public ClaimsController(ILogger<ClaimsController> logger, IClaimsService claimsService)
         {
             _logger = logger;
             _claimsService = claimsService;
         }
 
+        /// <summary>
+        /// The route to get all claims.
+        /// </summary>
+        /// <returns>A list of all existing claims.</returns>
         [HttpGet("Claims")]
         [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Claim>))]
@@ -27,6 +38,11 @@ namespace Claims.API.Controllers
             return Ok(result.Value);
         }
 
+        /// <summary>
+        /// The route to get a claim by its ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>The requested claim.</returns>
         [HttpGet("Claim/{id}")]
         [Produces("application/json")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Claim))]
